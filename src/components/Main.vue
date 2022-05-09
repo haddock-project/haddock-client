fe92090f815785af16e14ab61fdada71bec09f1f
Alexis <61119747+Alexis06030631@users.noreply.github.com>
2022-05-09 14:36:12 +0200

    Add description image

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,19 +1,38 @@
 <template>
   <div class="relative flex min-h-screen flex-col justify-end overflow-hidden">
     <div class="relative bg-white h-1/2 bg-opacity-50 shadow-xl sm:mx-auto sm:max-w-lg sm:rounded-t-[40px] p-10">
-      <div class="grid grid-cols-4 gap-16">
-        <div>
-          <img class="w-20" src="/images/app/google_doc.webp" alt="Google Doc">
+      <div class="relative flex flex-wrap -mx-3 overflow-hidden">
+
+        <div class="justify-end my-3 px-3 w-1/4 overflow-hidden">
+          <div class="overflow-hidden hover:scale-105 transition duration-500 cursor-pointer">
+            <div>
+              <img src="/images/app/google_doc.webp" alt="Google Doc">
+            </div>
+            <div class="p-2">
+              <button class="font-semibold items-center justify-center w-full text-white">Element</button>
+            </div>
+          </div>
         </div>
-        <div>
-          <img class="w-20" src="/images/app/google_doc.webp" alt="Google Doc">
+
+        <div class="my-3 px-3 w-1/4 overflow-hidden">
+          <div class="overflow-hidden hover:scale-105 transition duration-500 cursor-pointer">
+            <div>
+              <img src="/images/app/element.webp" alt="Element">
+            </div>
+            <div class="p-2">
+                <button class="font-semibold items-center justify-end w-full text-white">Element</button>
+            </div>
+          </div>
         </div>
-        <div>
-          <img class="w-20" src="/images/app/google_doc.webp" alt="Google Doc">
+
+        <div class="my-3 px-3 w-1/4 overflow-hidden">
+          <!-- Column Content -->
         </div>
-        <div>
-          <img class="w-20" src="/images/app/google_doc.webp" alt="Google Doc">
+
+        <div class="my-3 px-3 w-1/4 overflow-hidden">
+          <!-- Column Content -->
         </div>
+
       </div>
     </div>
   </div>
