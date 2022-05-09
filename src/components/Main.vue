5d010a2119329ffc1a86d3120545a30b3653ccbd
Alexis <61119747+Alexis06030631@users.noreply.github.com>
2022-05-09 22:54:17 +0200

    Add description && fix css

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,23 +1,22 @@
 <template>
   <div class="relative flex min-h-screen flex-col justify-end overflow-hidden">
-    <div class="relative bg-white h-1/2 bg-opacity-50 shadow-xl sm:mx-auto sm:max-w-lg sm:rounded-t-[40px] p-10">
-      <div class="relative flex flex-wrap -mx-3 overflow-hidden">
-
+    <div class="relative bg-white w-full bg-opacity-50 shadow-xl pb-14 sm:mx-auto sm:max-w-2xl sm:rounded-t-[40px] p-7">
+      <div class="relative flex flex-wrap">
         <div class="justify-end my-3 px-3 w-1/4 overflow-hidden">
           <div class="overflow-hidden hover:scale-105 transition duration-500 cursor-pointer">
-            <div>
-              <img src="/images/app/google_doc.webp" alt="Google Doc">
+            <div class="h-32 w-full flex justify-center">
+              <img class="h-full w-3/4" src="/images/app/google_doc.webp" alt="Google Doc">
             </div>
             <div class="p-2">
-              <button class="font-semibold items-center justify-center w-full text-white">Element</button>
+              <button class="font-semibold items-center justify-center w-full text-white">Google Doc</button>
             </div>
           </div>
         </div>
 
         <div class="my-3 px-3 w-1/4 overflow-hidden">
           <div class="overflow-hidden hover:scale-105 transition duration-500 cursor-pointer">
-            <div>
-              <img src="/images/app/element.webp" alt="Element">
+            <div class="h-32 w-full flex justify-center">
+              <img class="h-full w-full" src="/images/app/element.webp" alt="Element">
             </div>
             <div class="p-2">
                 <button class="font-semibold items-center justify-end w-full text-white">Element</button>
