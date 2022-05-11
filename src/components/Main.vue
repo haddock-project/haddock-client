88e30180ea078a82728387090f0053d79f97e9aa
Alexis <61119747+Alexis06030631@users.noreply.github.com>
2022-05-11 15:26:05 +0200

    Responsive icon && fix height, width of imgs and menu

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -16,48 +16,53 @@ setInterval(() => {
     <img src="/images/user.svg" alt="user">
   </div>
   <div class="absolute flex flex-col bottom-0 right-0 left-0 overflow-hidden">
-    <div class="relative bg-white w-full bg-opacity-50 shadow-xl pb-14 sm:mx-auto sm:max-w-2xl sm:rounded-t-[40px] p-7">
-      <div class="relative flex flex-wrap">
-        <div class="justify-end my-3 px-3 w-1/4 overflow-hidden">
-          <div class="overflow-hidden hover:scale-105 transition duration-500 cursor-pointer">
-            <div class="h-full w-full items-center justify-center flex justify-center">
-              <img class="h-3/4 w-3/4" src="/images/app/google_doc.webp" alt="Google Doc">
-            </div>
-            <div class="p-2">
-              <button class="font-semibold items-center justify-center w-full text-white">Google Doc</button>
-            </div>
-          </div>
-        </div>
-
-        <div class="my-3 px-3 w-1/4 overflow-hidden">
-          <div class="overflow-hidden hover:scale-105 transition duration-500 cursor-pointer">
-            <div class="h-full w-full items-center justify-center flex justify-center">
-              <img class="h-3/4 w-3/4" src="/images/app/element.webp" alt="Element">
-            </div>
-            <div class="p-2">
-                <button class="font-semibold items-center justify-end w-full text-white">Element</button>
+    <div class="relative bg-white w-full bg-opacity-50 shadow-xl sm:mx-auto sm:max-w-4xl sm:rounded-t-[40px] pt-7 px-7">
+      <div class="flex flex-wrap justify-center">
+        <div class="lg:w-1/5 w-1/2 md:w-1/4 h-1/2 p-4">
+          <div class="justify-end overflow-hidden">
+            <div class="overflow-hidden hover:scale-105 transition duration-500 cursor-pointer">
+              <div class="h-full w-full items-center justify-center flex justify-center">
+                <img class="h-3/5 w-3/5" src="/images/app/google_doc.webp" alt="Google Doc">
+              </div>
+              <div class="p-2">
+                <button class="font-semibold items-center justify-center w-full text-white">Google Doc</button>
+              </div>
             </div>
           </div>
         </div>
-
-        <div class="my-3 px-3 w-1/4 overflow-hidden">
-          <div class="overflow-hidden hover:scale-105 transition duration-500 cursor-pointer">
-            <div class="h-full w-full items-center justify-center flex justify-center">
-              <img class="h-3/4 w-3/4" src="/images/app/libreOffice.webp" alt="Libre Office">
-            </div>
-            <div class="p-2">
-              <button class="font-semibold items-center justify-end w-full text-white">Libre Office</button>
+        <div class="lg:w-1/5 w-1/2 md:w-1/4 h-1/2 p-4">
+          <div class="justify-end overflow-hidden">
+            <div class="overflow-hidden hover:scale-105 transition duration-500 cursor-pointer">
+              <div class="h-full w-full items-center justify-center flex justify-center">
+                <img class="h-3/5 w-3/5" src="/images/app/element.webp" alt="Element">
+              </div>
+              <div class="p-2">
+                <button class="font-semibold items-center justify-center w-full text-white">Element</button>
+              </div>
             </div>
           </div>
         </div>
-
-        <div class="my-3 px-3 w-1/4 overflow-hidden">
-          <div class="overflow-hidden hover:scale-105 transition duration-500 cursor-pointer">
-            <div class="h-full w-full items-center justify-center flex justify-center">
-              <img class="h-3/4 w-3/4" src="/images/app/download-icon.webp" alt="Download icon">
+        <div class="lg:w-1/5 w-1/2 md:w-1/4 h-1/2 p-4">
+          <div class="justify-end overflow-hidden">
+            <div class="overflow-hidden hover:scale-105 transition duration-500 cursor-pointer">
+              <div class="h-full w-full items-center justify-center flex justify-center">
+                <img class="h-3/5 w-3/5" src="/images/app/libreOffice.webp" alt="Libre Office">
+              </div>
+              <div class="p-2">
+                <button class="font-semibold items-center justify-center w-full text-white">Libre Office</button>
+              </div>
             </div>
-            <div class="p-2">
-              <button class="font-semibold items-center text-sm justify-end w-full text-white">Download more Application</button>
+          </div>
+        </div>
+        <div class="lg:w-1/5 w-1/2 md:w-1/4 h-1/2 p-4">
+          <div class="justify-end overflow-hidden">
+            <div class="overflow-hidden hover:scale-105 transition duration-500 cursor-pointer">
+              <div class="h-full w-full items-center justify-center flex justify-center">
+                <img class="h-3/5 w-3/5" src="/images/app/download-icon.webp" alt="Download icon">
+              </div>
+              <div class="p-2">
+                <button class="font-semibold items-center justify-center w-full text-white">Download more Application</button>
+              </div>
             </div>
           </div>
         </div>
