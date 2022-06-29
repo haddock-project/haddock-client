1a1c00308142d0890c9e7200b744c75a93740bc2
Alexis <61119747+Alexis06030631@users.noreply.github.com>
2022-06-29 20:33:28 +0200

    auto load all translation files

--- src/i18n.js
+++ src/i18n.js
@@ -1,10 +1,19 @@
 import { createI18n } from 'vue-i18n'
-import en from './translations/en-US.json'
 
-export default new createI18n({
-    locale: 'en-US',
-    fallbackLocale: 'en-US',
-    messages: {
-        'en-US': en
+async function LoadLanguages() {
+    let lang = {}
+    // get all languages in the translations folder
+    const files = import.meta.glob('./translations/*.json')
+    for (const path in files) {
+        await files[path]().then((mod) => {
+            console.log('lang loaded', path, mod.default)
+            Object.assign(lang, {[path.split('/').pop().split('.')[0]]: mod.default})
+        })
     }
-})
+    return new createI18n({
+        locale: 'en-US',
+        fallbackLocale: 'en-US',
+        messages: lang
+    })
+}
+export default await LoadLanguages()
