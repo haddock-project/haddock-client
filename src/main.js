b419c5c33cc5d93ff86a3e1115931e30820be621
Alexis <61119747+Alexis06030631@users.noreply.github.com>
2022-06-30 13:48:28 +0200

    add pages router

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
 import Default from './Default.vue'
 import './assets/tailwind-init.css'
 import Toast from "vue-toastification";
 import "vue-toastification/dist/index.css";
 import i18n from './i18n'
+import router from './router'
 
 // create the app
 const app = createApp(Default)
@@ -17,5 +19,6 @@ const options = {
 
 // register the plugins
 app.use(Toast, options);
+app.use(router);
 app.use(i18n);
 app.mount('#app')
---
+++ src/router.js
@@ -0,0 +1,16 @@
+import {createRouter, createWebHistory} from "vue-router";
+
+// Create routes
+const routes = [
+    { path: '/', name: "Home", component: import('./views/Main.vue') },
+    { path: '/settings', name: "Settings", component: import('./views/Settings.vue') },
+]
+
+
+// Create a new router instance and pass the `routes` option
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+export default router;
