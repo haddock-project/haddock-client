b06fcf33b9af0c12cb2e9e97a58dc6dcd2942df0
Alexis <61119747+Alexis06030631@users.noreply.github.com>
2022-07-01 17:34:28 +0200

    change bg menu page

--- src/Default.vue
+++ src/Default.vue
@@ -10,7 +10,7 @@ let conn = ref(true)
 let connection
 function connect() {
   console.log("Starting connection to WebSocket Server")
-  connection = new WebSocket("ws://localhost:8080/ws/")
+  connection = new WebSocket("ws://localhost:8080/api/ws/")
   connection.onmessage = function(event) {
     console.log(event);
   }
@@ -83,10 +83,10 @@ window.onclick = function(event) {
 
       <hr class="border-gray-200">
 
-      <router-link to="/" class="block px-4 py-3 text-sm text-gray-600 capitalize transition-colors duration-200 transform hover:bg-gray-100">
+      <router-link to="/" :class="$route.name === 'Home'? 'bg-gray-100 cursor-not-allowed' : 'hover:bg-gray-100'" class="block px-4 py-3 text-sm text-gray-600 capitalize transition-colors duration-200 transform">
         Home
       </router-link>
-      <router-link to="/settings" class="block px-4 py-3 text-sm text-gray-600 capitalize transition-colors duration-200 transform hover:bg-gray-100">
+      <router-link to="/settings" :class="$route.name === 'Settings'? 'bg-gray-100 cursor-not-allowed' : 'hover:bg-gray-100'" class="block px-4 py-3 text-sm text-gray-600 capitalize transition-colors duration-200 transform">
         Settings
       </router-link>
 
@@ -105,13 +105,4 @@ window.onclick = function(event) {
   <!-- Start App -->
   <router-view></router-view>
   <!-- End App -->
-  <!-- Reconnect ws button -->
-  <div v-if="!conn.value" @click="connect()" class="fixed right-0 bottom-0 p-5">
-    <button type="button" title="Retry to connect to the ws" class="group inline-flex items-center justify-center w-auto px-3 py-2 space-x-2 text-sm font-medium text-white transition bg-red-700 rounded appearance-none cursor-pointer select-none hover:bg-red-800">
-      <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 group-hover:animate-spin" viewBox="0 0 20 20" fill="currentColor">
-        <path fill-rule="evenodd" d="M4 2a1 1 0 011 1v2.101a7.002 7.002 0 0111.601 2.566 1 1 0 11-1.885.666A5.002 5.002 0 005.999 7H9a1 1 0 010 2H4a1 1 0 01-1-1V3a1 1 0 011-1zm.008 9.057a1 1 0 011.276.61A5.002 5.002 0 0014.001 13H11a1 1 0 110-2h5a1 1 0 011 1v5a1 1 0 11-2 0v-2.101a7.002 7.002 0 01-11.601-2.566 1 1 0 01.61-1.276z" clip-rule="evenodd" />
-      </svg>
-    </button>
-  </div>
-  <!-- End Reconnect ws button -->
 </template>
